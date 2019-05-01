418d1c2d2161f17472409ea33c407e6df55d6a5b
Franch Fry <crfranchi93@gmail.com>
2019-05-01 13:50:35 -0700

    practicing .map() method

---
+++ app.js
@@ -0,0 +1,14 @@
+var beertype = [
+    {id: 1, name: "Sculpin", Brewery, "Ballast Point"},
+    {id: 2, name: "Delicious IPA", Brewery, "Stone"},
+    {id: 3, name: "Logan Lager", Brewery, "IronFist"},
+    {id: 4, name: "Cali Creamin", Brewery, "Mother Earth"},
+    {id: 5, name: "394", Brewery, "AleSmith Brewing"},
+]
+
+// Using the .map method
+var mapSimpleArray = beertype.map(function(beer) {
+    return item;
+  });
+
+  console.log(mapSimpleArray);
